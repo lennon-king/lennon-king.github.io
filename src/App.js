82c2c948ff60a29590b9745af3999d686155ee8b
Lennon King <lennon.king@ufl.edu>
2025-02-03 12:12:06 -0500

    Updated navbar styling and structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+
+import Header from "./Header.js"
+import Footer from "./Footer.js"
+import Body from "./Body.js"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+
+export default App;
