d142a4bcaf0ba9e2f417e96b3310c6220e2afe7d
Lennon King <116830939+lennon-king@users.noreply.github.com>
2025-02-19 17:49:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home.js';
 import Contact from './pages/Contact.js';
 import Projects from './pages/Projects.js';
