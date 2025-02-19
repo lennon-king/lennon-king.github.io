579259ccec49472a64bbde8b9c7a2f5eb5a077a2
Lennon King <116830939+lennon-king@users.noreply.github.com>
2025-02-19 18:00:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/#/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/#/contact" element={<Contact />} />
         <Route path="/#/projects" element={<Projects />} />
         <Route path="/#/SHPE-UF-Offical-App" element={<SHPEUFOfficalApp />} />
