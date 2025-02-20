8a30e7799ef6b5e5dd91a8938c3f9bcee9bcbec1
Lennon King <116830939+lennon-king@users.noreply.github.com>
2025-02-20 13:40:43 -0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,24 @@
-
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+    const navigate = useNavigate();
+
     return (
-        <>
-            <div className="intro-background">
-                {/* Main Header Content (Intro & About Me) */}
-                <header>
-                    <div className="intro-text">
-                        Hello, I'm Lennon: a student making memorable user experiences.
-                    </div>
-                    <div className="about-me-text">
-                        Lead UI/UX Designer for SHPE UF and a student at the University of Florida studying Computer Science
-                        with a minor in Digital Arts and Sciences.
-                    </div>
-                    <button className="contact-me-button" onClick={() => window.location.href = "contact"}>
-                        Contact Me
-                    </button>
-                </header>
-            </div>
-        </>
+        <div className="intro-background">
+            <header>
+                <div className="intro-text">
+                    Hello, I'm Lennon: a student making memorable user experiences.
+                </div>
+                <div className="about-me-text">
+                    Lead UI/UX Designer for SHPE UF and a student at the University of Florida studying Computer Science
+                    with a minor in Digital Arts and Sciences.
+                </div>
+                <button className="contact-me-button" onClick={() => navigate("/contact")}>
+                    Contact Me
+                </button>
+            </header>
+        </div>
     );
 }
 
