f4a0a2b849a33bc021d6ca0ec2e24ab2abf9c929
Lennon King <116830939+lennon-king@users.noreply.github.com>
2025-02-20 21:34:59 -0500

    Update SHPEUFApp.js

--- src/pages/SHPEUFApp.js
+++ src/pages/SHPEUFApp.js
@@ -1,22 +1,21 @@
-import logo from '../logo.svg';
 import '../App.css';
 import '../style.css';
+import { useEffect } from "react";
 
-import Header from "../Header.js"
-import ContactMe from "../ContactMe.js"
 import Footer from "../Footer.js"
 import Body from "../Body.js"
 import Navbar from "../Navbar.js"
 import Background from '../Background.js';
 import ProjectText from "../SHPEAppProjectText.js"
-import Contact from '../ContactMe.js';
-import SHPEUFOfficialAppPage from '../SHPEUFOfficialAppPage.js';
-import mockup_1 from "../SHPE_app_mockup_1.jpeg"
 import SHPEUFAppHeader from "../SHPEUFAppHeader.js";
 
 document.body.style.overflow = 'auto';
 
 function Home() {
+    useEffect(() => {
+        {document.body.style.overflow = 'auto'};
+    }, []);
+
     return (
         <div>
             <Navbar />
